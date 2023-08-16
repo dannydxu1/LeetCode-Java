3e18e50208bf05109c866b640c2131e25b9072ef
Danny <dannydxu1@gmail.com>
2023-08-15 21:51:46 -0500

    ez

--- 217.contains-duplicate.java
+++ 217.contains-duplicate.java
@@ -5,9 +5,21 @@
  */
 
 // @lc code=start
+import java.util.*;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        hi
+        //slow solution: O(N^2), use nested for loop and compare each possible pair
+        //fast approach: O(NlogN), since order doesn't matter, sort the array, and get it in one go
+        Arrays.sort(nums);
+        Set<Integer> hs = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++ ) {
+            int num = nums[i];
+            if (hs.contains(num))
+                return true;
+            hs.add(num);
+        }
+        return false;
     }
 }
 // @lc code=end
